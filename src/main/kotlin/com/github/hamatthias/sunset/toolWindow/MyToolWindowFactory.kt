fb8898a09e0d5f7789fe093ac3bdd96321d6c075
GitHub Action <action@github.com>
2024-12-03 19:49:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/hamatthias/sunset/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hamatthias/sunset/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hamatthias.sunset.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hamatthias.sunset.MyBundle
+import com.github.hamatthias.sunset.services.MyProjectService
 import javax.swing.JButton
 
 
