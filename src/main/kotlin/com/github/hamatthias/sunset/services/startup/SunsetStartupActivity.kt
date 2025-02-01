36afec9a3c8b086e01c878666a6d1e9e749e9aa4
mhahn <mhahn@suh-solutions.eu>
2025-02-01 11:30:29 +0100

    Add startup checker to switch theme if necessary and schedule new change with the scheduler

---
+++ src/main/kotlin/com/github/hamatthias/sunset/services/startup/SunsetStartupActivity.kt
@@ -0,0 +1,35 @@
+package com.github.hamatthias.sunset.services.startup
+
+import com.github.hamatthias.sunset.services.theme.ThemeChanger
+import com.github.hamatthias.sunset.services.theme.ThemeChangerSchedulingService
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * This class is responsible for taking detached action on Application startup.
+ *
+ * The action taken consist of applying a theme if it needs to be applied because its time to change is overdue
+ * and the second action is to schedule the next theme change.
+ *
+ * @see [Application Startup Documentation](https://plugins.jetbrains.com/docs/intellij/plugin-components.html#project-open)
+ * @see [Spaces Break KDoc URLs](https://youtrack.jetbrains.com/issue/KTIJ-12442/KDoc-Support-clickable-URLs-highlighting-navigation)
+ */
+class SunsetStartupActivity : ProjectActivity {
+
+  override suspend fun execute(project: Project) {
+    withContext(Dispatchers.EDT) {
+      // Apply theme directly on startup
+      val themeChanger = ThemeChanger()
+      themeChanger.applyThemeIfNeeded()
+
+      // Schedule the next theme change
+      val service = project.service<ThemeChangerSchedulingService>()
+      val changeTime = themeChanger.getNextThemeChange()
+      service.scheduleThemeChange(changeTime) { themeChanger.switchTheme() }
+    }
+  }
+}
---
+++ src/main/kotlin/com/github/hamatthias/sunset/services/theme/ThemeChangerSchedulingService.kt
@@ -0,0 +1,37 @@
+package com.github.hamatthias.sunset.services.theme
+
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.*
+import java.time.Duration
+import java.time.LocalTime
+
+/**
+ * Launches a [CoroutineScope] to change a theme at a specific time.
+ *
+ * fdsjgsghaslghsgfdf
+ * dfdfdsffd
+ */
+@Service(Service.Level.PROJECT)
+class ThemeChangerSchedulingService(
+  private val project: Project,
+  private val cs: CoroutineScope
+) {
+
+  // There should be only one Job at a time
+  private lateinit var themeChangeJob : Job
+
+  fun scheduleThemeChange(executionTime: LocalTime, themeChangeTask: () -> Unit) {
+    val now = LocalTime.now()
+    if (executionTime.isAfter(now)) {
+      val delay = Duration.between(now, executionTime).toMillis()
+      themeChangeJob = cs.launch {
+        withContext(Dispatchers.EDT) {
+          delay(delay)
+          themeChangeTask()
+        }
+      }
+    }
+  }
+}
