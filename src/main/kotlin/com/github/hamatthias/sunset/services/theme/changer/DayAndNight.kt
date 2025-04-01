68100d2159706c7dda18ab532af54374382dd877
mhahn <mhahn@suh-solutions.eu>
2025-04-01 20:20:13 +0200

    Retrieve SunsetSettings via service API of IntelliJ Platform

--- src/main/kotlin/com/github/hamatthias/sunset/services/theme/changer/DayAndNight.kt
+++ src/main/kotlin/com/github/hamatthias/sunset/services/theme/changer/DayAndNight.kt
@@ -4,14 +4,13 @@ import com.github.hamatthias.sunset.services.theme.ThemeGatherer
 import com.github.hamatthias.sunset.settings.SunsetSettings
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.laf.UIThemeLookAndFeelInfo
+import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import java.time.LocalTime
 
 object DayAndNight : ThemeChanger {
 
   private val logger = logger<DayAndNight>()
-
-  private val settings = SunsetSettings.getInstance()
 
   override fun applyTheme() {
     val themeToApply = getNextTheme()
@@ -25,6 +24,7 @@ object DayAndNight : ThemeChanger {
   }
 
   override fun getNextThemeChange() : LocalTime {
+    val settings = service<SunsetSettings>()
     val timeToDayTheme = LocalTime.parse(settings.state.timeToDayTheme)
     val timeToNightTheme = LocalTime.parse(settings.state.timeToNightTheme)
     val now = LocalTime.now()
@@ -36,6 +36,7 @@ object DayAndNight : ThemeChanger {
   }
 
   override fun getNextTheme(): UIThemeLookAndFeelInfo {
+    val settings = service<SunsetSettings>()
     val timeToDayTheme = LocalTime.parse(settings.state.timeToDayTheme)
     val timeToNightTheme = LocalTime.parse(settings.state.timeToNightTheme)
     val now = LocalTime.now()
