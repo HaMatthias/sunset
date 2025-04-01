7e56619f731ec03ff7d94198e7e375c0609641b3
mhahn <mhahn@suh-solutions.eu>
2025-04-01 20:27:35 +0200

    Bind strategy implementation to ThemeChangingStrategies.kt
    This Enum will be stored into the storage and thus the proper implementation can be received directly via settings.

--- src/main/kotlin/com/github/hamatthias/sunset/services/theme/changer/ThemeChangingStrategies.kt
+++ src/main/kotlin/com/github/hamatthias/sunset/services/theme/changer/ThemeChangingStrategies.kt
@@ -2,8 +2,11 @@ package com.github.hamatthias.sunset.services.theme.changer
 
 import com.github.hamatthias.sunset.settings.SettingsBundle
 
-enum class ThemeChangingStrategies(val displayName: String) {
-  LOCATION(SettingsBundle.setting("label.radio.strategy.location")),
-  DAY_AND_NIGHT(SettingsBundle.setting("label.radio.strategy.time")),
-  RANDOM(SettingsBundle.setting("label.radio.strategy.random"))
+enum class ThemeChangingStrategies(
+  val displayName: String,
+  val strategyImplementation: ThemeChanger
+) {
+  LOCATION(SettingsBundle.setting("label.radio.strategy.location"), Location),
+  DAY_AND_NIGHT(SettingsBundle.setting("label.radio.strategy.time"), DayAndNight),
+  RANDOM(SettingsBundle.setting("label.radio.strategy.random"), DiceRoller)
 }
